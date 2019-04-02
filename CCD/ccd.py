2c73ba347afec912cbd402465b238eb34c4b2168
NoSegfault <jerrynjzhang@gmail.com>
2019-04-02 15:14:19 -0500

    change function_tensor

--- CCD/ccd.py
+++ CCD/ccd.py
@@ -5,35 +5,36 @@ import time
 glob_comm = ctf.comm()
 import numpy as np
 
-def function_tensor(I,J,K,sparsity):
-    #N = 5
-    #n = 51
-    #L = 100
-    #nsample = 10*N*n*L #10nNL = 255000
-    n = I
+def function_tensor(I, J, K, sparsity):
+    # N = 5
+    # n = 51
+    # L = 100
+    # nsample = 10*N*n*L #10nNL = 255000
 
-    v = np.linspace(-1,1,n)
-    #v = np.arange(1,n+1)
-    v = ctf.astensor(v**2)
-    
-    v2 = ctf.tensor(n)
-    v2 = v
+    T = ctf.tensor((I, J, K))
+    T2 = ctf.tensor((I, J, K))
 
-    T = ctf.tensor((I,J,K))
-    T.fill_sp_random(1,1,sparsity)
-    #T = ctf.exp(-1 * ctf.power(ctf.power(T,2),0.5))  # x = exp(-sqrt(x^2)) 
+    T.fill_sp_random(1, 1, sparsity)
+    # T = ctf.exp(-1 * ctf.power(ctf.power(T,2),0.5))  # x = exp(-sqrt(x^2))
 
-    T2 = ctf.tensor((I,J,K))
-    T2.i("ijk") << T.i("ijk") * v2.i("i")
-    T2.i("ijk") << T.i("ijk") * v2.i("j")
-    T2.i("ijk") << T.i("ijk") * v2.i("k")
-    
-    T2 = ctf.power(T2,0.5)
+    sizes = [I, J, K]
+    index = ["i", "j", "k"]
 
+    for i in range(3):
+        n = sizes[i]
+        v = np.linspace(-1, 1, n)
+        # v = np.arange(1,n+1)
+        v = ctf.astensor(v ** 2)
+
+        v2 = ctf.tensor(n)
+        v2 = v
+        T2.i("ijk") << T.i("ijk") * v2.i(index[i])
+
+    T2 = ctf.power(T2, 0.5)
     T2 = (-1.0) * T2
-    
-    #T2 = ctf.exp(T2)
-    
+
+    # T2 = ctf.exp(T2)
+
     return T2
 
 def getOmega(T):
