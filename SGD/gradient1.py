fb88f656239074df121b7e547a20ec317ec631c1
jasonzhang928 <zhangsiyuan929@gmail.com>
2019-04-12 16:33:57 -0500

    bug

--- SGD/gradient1.py
+++ SGD/gradient1.py
@@ -10,11 +10,6 @@ glob_comm = ctf.comm()
 INDEX_STRING = "ijklmnopq"
 
 
-def getOmega(T):
-    omegactf = ((T > 0) * ctf.astensor(1.))
-    return omegactf
-
-
 def stepdense_updateU(T, U, V, W, Lambda, I, J, K, r, R, stepSize):
     R = ctf.tensor((I, J, K), sp=True)
     R.i("ijk") << T.i("ijk") - U.i("iu") * V.i("ju") * W.i("ku")
@@ -125,15 +120,15 @@ def sparse_update(T, factors, Lambda, sizes, rank, stepSize):
 
     for i in range(dimension):
         tup_list = [factors[i].i(indexes[i] + "r") for i in range(dimension)]
-        R.i(indexes) << T.i(indexes) - omega.i(indexes) * reduce(lambda x, y: x * y, tup_list)
+        #R.i(indexes) << T.i(indexes) - omega.i(indexes) * reduce(lambda x, y: x * y, tup_list)
+        R.i(indexes) << T.i(indexes) - ctf.TTTP(omega, factors).i(indexes)
         H = ctf.tensor(tuple((sizes[:i] + sizes[i + 1:] + [rank])))
         H.i(indexes[:i] + indexes[i + 1:] + "r") << reduce(lambda x, y: x * y, tup_list[:i] + tup_list[i + 1:])
         factors[i].i(indexes[i] + "r") << - stepSize * (
-                    2 * Lambda * factors[i].i(indexes[i] + "r") - omega.i(indexes) * R.i(indexes) * H.i(
-                indexes[:i] + indexes[i + 1:] + "r"))
+                    2 * Lambda * factors[i].i(indexes[i] + "r") - H.i(indexes[:i] + indexes[i + 1:] + "r")) * omega.i(indexes) * R.i(indexes)
         if i < dimension - 1:
             R.set_zero()
-    return ctf.vecnorm(R) + (sum([ctf.vecnorm(f) for f in factors])) * Lambda
+    #return ctf.vecnorm(R) + (sum([ctf.vecnorm(f) for f in factors])) * Lambda
 
 
 def sparse_update_steps(T, factors, Lambda, sizes, rank, stepSize):
@@ -247,35 +242,39 @@ def test_sparse_GD(T, U, V, W, Lambda, omega, I, J, K, r, stepSize):
 def sparse_SGD(T, U, V, W, Lambda, omega, I, J, K, r, stepSize, sample_rate):
     iteration_count = 0
     R = ctf.tensor((I, J, K), sp=True)
-    R.i("ijk") << T.i("ijk") - U.i("iu") * V.i("ju") * W.i("ku") * omega.i("ijk")
+    R.i("ijk") << T.i("ijk") - ctf.TTTP(omega, [U, V, W]).i("ijk")
+    # R.i("ijk") << T.i("ijk") - U.i("iu") * V.i("ju") * W.i("ku") * omega.i("ijk")
     curr_err_norm = ctf.vecnorm(R) + (ctf.vecnorm(U) + ctf.vecnorm(V) + ctf.vecnorm(W)) * Lambda
     norm = [curr_err_norm]
-    work_cycle = int(0.5 / sample_rate)
+    work_cycle = int(1.0 / sample_rate)
     step = stepSize * 0.5
     starting_time = time.time()
     while True:
+        iteration_count += 1
         sampled_T = T.copy()
         sampled_T.sample(sample_rate)
+
         sparse_update(sampled_T, [U, V, W], Lambda, [I, J, K], r, stepSize * 0.5 + step)
         step *= 0.99
         sampled_T.set_zero()
 
         if iteration_count % work_cycle == 0:
             R.set_zero()
-            R.i("ijk") << T.i("ijk") - U.i("iu") * V.i("ju") * W.i("ku") * omega.i("ijk")
+            #R.i("ijk") << T.i("ijk") - U.i("iu") * V.i("ju") * W.i("ku") * omega.i("ijk")
+            R.i("ijk") << T.i("ijk") - ctf.TTTP(omega, [U, V, W]).i("ijk")
             diff_norm = ctf.vecnorm(R)
             next_err_norm = diff_norm + (ctf.vecnorm(U) + ctf.vecnorm(V) + ctf.vecnorm(W)) * Lambda
             #print(curr_err_norm, next_err_norm, diff_norm)
-            print(diff_norm, time.time() - starting_time)
+            if ctf.comm().rank() == 0:
+                print(diff_norm, time.time() - starting_time)
 
             if abs(curr_err_norm - next_err_norm) < .001 or iteration_count > work_cycle * 50:
                 break
 
             curr_err_norm = next_err_norm
             norm.append(curr_err_norm)
-        iteration_count += 1
-
-    print("Number of iterations: ", iteration_count)
+    if ctf.comm().rank() == 0:
+        print("Number of iterations: ", iteration_count)
     return norm
 
 
@@ -311,55 +310,10 @@ def test_sparse_SGD(T, U, V, W, Lambda, omega, I, J, K, r, stepSize, sample_rate
             norm.append(curr_err_norm)
         iteration_count += 1
 
-    print("Number of iterations: ", iteration_count)
+    #print("Number of iterations: ", iteration_count)
     return norm, stepSizes
 
 
-# def sparse_SGD(T,U,V,W,Lambda,omega,I,J,K,r,stepSize,sample_rate):
-#     iteration_count = 0
-#     E = ctf.tensor((I,J,K), sp = True)
-#     R = ctf.tensor((I,J,K), sp = True)
-#     R.i("ijk") << T.i("ijk") - omega.i("ijk")*U.i("iu")*V.i("ju")*W.i("ku")
-#     sampled_T = ctf.tensor((I,J,K), sp = True)
-#     curr_err_norm = ctf.vecnorm(R) + (ctf.vecnorm(U) + ctf.vecnorm(V) + ctf.vecnorm(W))*Lambda
-#     norm = [curr_err_norm]
-#     while True:
-#         random = ctf.tensor((I,J,K))
-#         random.fill_random(0, 1)
-#         random = ((random < sample_rate)*ctf.astensor(1.))
-#         sampled_T = T * random
-#         #sampled_T.sample(sample_rate)
-#         sampled_omega = getOmega(sampled_T)
-#         E.i("ijk") << sampled_T.i("ijk") - sampled_omega.i("ijk") * U.i("iu") * V.i("ju") * W.i("ku")
-#         U = sparse_updateU(sampled_T,U,V,W,Lambda,sampled_omega,I,J,K,r,E,stepSize)
-#         E.set_zero()
-#         E.i("ijk") << sampled_T.i("ijk") - sampled_omega.i("ijk") * U.i("iu") * V.i("ju") * W.i("ku")
-#         V = sparse_updateV(sampled_T,U,V,W,Lambda,sampled_omega,I,J,K,r,E,stepSize)
-#         E.set_zero()
-#         E.i("ijk") << sampled_T.i("ijk") - sampled_omega.i("ijk") * U.i("iu") * V.i("ju") * W.i("ku")
-#         W = sparse_updateW(sampled_T,U,V,W,Lambda,sampled_omega,I,J,K,r,E,stepSize)
-#         E.set_zero()
-#         sampled_T.set_zero()
-#
-#         if iteration_count % 10 == 0:
-#             R.set_zero()
-#             R.i("ijk") << T.i("ijk") - omega.i("ijk")*U.i("iu")*V.i("ju")*W.i("ku")
-#             diff_norm = ctf.vecnorm(R)
-#             next_err_norm = diff_norm + (ctf.vecnorm(U) + ctf.vecnorm(V) + ctf.vecnorm(W))*Lambda
-#             print(curr_err_norm, next_err_norm)
-#             print(diff_norm)
-#             print(ctf.sum(sampled_omega))
-#
-#             if abs(curr_err_norm - next_err_norm) < .001 or iteration_count > 1000:
-#                 break
-#
-#             curr_err_norm = next_err_norm
-#             norm.append(curr_err_norm)
-#
-#         iteration_count += 1
-#
-#     print("Number of iterations: ", iteration_count)
-#     return norm
 
 def dense_SGD(T, U, V, W, Lambda, I, J, K, r, stepSize, sample_rate):
     iteration_count = 0
@@ -406,23 +360,25 @@ def function_tensor(I, J, K, sparsity):
     # n = 51
     # L = 100
     # nsample = 10*N*n*L #10nNL = 255000
-    n = I
-
-    v = np.linspace(-1, 1, n)
-    # v = np.arange(1,n+1)
-    v = ctf.astensor(v ** 2)
-
-    v2 = ctf.tensor(n, sp=True)
-    v2 = v
 
     T = ctf.tensor((I, J, K), sp=True)
+    T2 = ctf.tensor((I, J, K), sp=True)
+
     T.fill_sp_random(1, 1, sparsity)
     # T = ctf.exp(-1 * ctf.power(ctf.power(T,2),0.5))  # x = exp(-sqrt(x^2))
 
-    T2 = ctf.tensor((I, J, K), sp=True)
-    T2.i("ijk") << T.i("ijk") * v2.i("i")
-    T2.i("ijk") << T.i("ijk") * v2.i("j")
-    T2.i("ijk") << T.i("ijk") * v2.i("k")
+    sizes = [I, J, K]
+    index = ["i", "j", "k"]
+
+    for i in range(3):
+        n = sizes[i]
+        v = np.linspace(-1, 1, n)
+        # v = np.arange(1,n+1)
+        v = ctf.astensor(v ** 2)
+
+        v2 = ctf.tensor(n, sp=True)
+        v2 = v
+        T2.i("ijk") << T.i("ijk") * v2.i(index[i])
 
     T2 = ctf.power(T2, 0.5)
     T2 = (-1.0) * T2
@@ -431,47 +387,38 @@ def function_tensor(I, J, K, sparsity):
 
     return T2
 
+def getOmega(T):
+    [inds, data] = T.read_local_nnz()
+    data[:] = 1.
+    Omega = ctf.tensor(T.shape,sp=True)
+    Omega.write(inds,data)
+    return Omega
+
+
+
 def main():
-    size_lb = 40
-    size_ub = 40
+    s = 800
+    sparsity = 100
     r = 5
-    sparsity = .05
     regParam = 0.00001
     stepSize = 0.01
-    sample_rate = 0.01
+    sample_rate = 0.05
 
-    random.seed(42)
-    I = random.randint(size_lb, size_ub)
-    J = random.randint(size_lb, size_ub)
-    K = random.randint(size_lb, size_ub)
-
-    target_tensor = ctf.tensor((I, J, K))
-    target_tensor.fill_random(0, 1)
-
-    ctf.random.seed(42)
-    target_U = ctf.random.random((I, r))
-    target_V = ctf.random.random((J, r))
-    target_W = ctf.random.random((K, r))
+    I = s
+    J = s
+    K = s
     T = ctf.tensor((I, J, K), sp=True)
-    #T.i("ijk") << target_U.i("ir") * target_V.i("jr") * target_W.i("kr")
-    T.fill_sp_random(0,1,sparsity)
-    #rand = ctf.tensor((I,J,K))
-    #rand.fill_random(0, 1)
-    #rand = ((rand < sparsity)*ctf.astensor(1.))
-    #T = T * rand
-    omega = getOmega(T)
+    T.fill_sp_random(0., 1., 1. / sparsity)
+    # T.read_from_file("T.txt")
+    Omega = getOmega(T)
+    if ctf.comm().rank() == 0:
+        print(T.sum(), Omega.sum())
+    #T = function_tensor(I, J, K, sparsity)
     U = ctf.random.random((I, r))
     V = ctf.random.random((J, r))
     W = ctf.random.random((K, r))
-    noise = 0.3
-    target_U.i("ir") << U.i("ir") * noise
-    target_V.i("jr") << V.i("jr") * noise
-    target_W.i("kr") << W.i("kr") * noise
-    T.write_to_file("T.txt")
-    T1 = ctf.tensor((I, J, K), sp=True)
-    T1.read_from_file("T.txt")
-    print(ctf.vecnorm(T - T1))
 
-    sparse_SGD(T, ctf.tensor(copy=target_U), ctf.tensor(copy=target_V), ctf.tensor(copy=target_W), regParam, omega, I, J, K, r, stepSize, sample_rate)
+    #T.write_to_file("T.txt")
+    sparse_SGD(T, ctf.tensor(copy=U), ctf.tensor(copy=V), ctf.tensor(copy=W), regParam, Omega, I, J, K, r, stepSize, sample_rate)
 
 main()
