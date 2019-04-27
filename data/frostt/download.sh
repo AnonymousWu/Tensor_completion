d9abe20214ad5129cf10143c0c718e9c35c1ced7
NoSegfault <jerrynjzhang@gmail.com>
2019-04-27 17:44:47 -0500

    add script to download tensors

---
+++ data/frostt/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/nell/nell-2.tns.gz
+#gunzip < nell-2.tns.gz > nell-2.tns
+
+wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/nell/nell-1.tns.gz
+gunzip < nell-1.tns.gz > nell-1.tns
+
+#wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/amazon/amazon-reviews.tns.gz
+#gunzip < amazon-reviews.tns.gz > amazon-reviews.tns
