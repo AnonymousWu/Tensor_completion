37637a89e1216fac1e2254ef96f9c2544e3fec9f
NoSegfault <jerrynjzhang@gmail.com>
2019-04-27 19:37:55 -0500

    remove nell-1

--- data/frostt/download.sh
+++ data/frostt/download.sh
@@ -3,8 +3,11 @@
 #wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/nell/nell-2.tns.gz
 #gunzip < nell-2.tns.gz > nell-2.tns
 
-wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/nell/nell-1.tns.gz
-gunzip < nell-1.tns.gz > nell-1.tns
-
 #wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/amazon/amazon-reviews.tns.gz
 #gunzip < amazon-reviews.tns.gz > amazon-reviews.tns
+
+wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/patents/patents.tns.gz
+gunzip < patents.tns.gz > patents.tns
+
+wget https://s3.us-east-2.amazonaws.com/frostt/frostt_data/reddit-2015/reddit-2015.tns.gz
+gunzip < reddit-2015.tns.gz > reddit-2015.tns
