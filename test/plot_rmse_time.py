45afdead0150c1fd67a0069acc59dbdfe6337006
NoSegfault <jerrynjzhang@gmail.com>
2019-07-05 23:41:32 -0700

    Create plotting code for RMSE vs time

---
+++ test/plot_rmse_time.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+method = []
+rmse = []
+time = []
+
+def parse_output(file_name):
+	num_method = 0
+	with open(file_name, 'r') as f:
+        	for line in f:
+                	if line[2:12] == 'Performing':
+                        	method.append(line.split('seconds')[1][4:-3])
+                        	rmse.append([])
+                        	time.append([])
+                        	num_method += 1
+                	if line[2:6] == 'RMSE':
+                        	rmse[num_method-1].append(float(line.split(':')[1][1:-3]))
+                        	time[num_method-1].append(float(line.split(',')[1][1:]))
+
+
+def plot_each():
+	for i in range(len(method)):
+		plt.plot(rmse[i], time[i], 'ro')
+		plt.plot(rmse[i], time[i])
+		plt.title(method[i])
+		plt.xlabel('Time')
+		plt.ylabel('RMSE')
+		plt.show()
+
+def plot_together():
+	for i in range(len(method)):
+		plt.plot(rmse[i], time[i], 'ro')
+		plt.plot(rmse[i], time[i], label=method[i])
+	plt.xlabel('Time')
+	plt.ylabel('RMSE')
+	plt.legend()
+	plt.show()
+
+parse_output('out.txt')
+plot_each()
+plot_together()
